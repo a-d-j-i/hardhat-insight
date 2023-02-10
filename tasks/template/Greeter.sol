fcd50047dc9577cd3ffd7921f79c8952b70ff56a
Andres Adjimann <aadjiman@gmail.com>
2023-02-10 15:07:40 -0300

    feat: add support for interfaces

--- tasks/template/Greeter.sol
+++ tasks/template/Greeter.sol
@@ -5,13 +5,20 @@ import "./Base1.sol";
 import "./Base2.sol";
 import "hardhat/console.sol";
 
+interface IFace {
+    function someFunc() external;
+}
+
 contract Greeter is Base1, Base2 {
     struct SG {uint16 a; uint16 b; uint256 c;}
+
+    struct SG1 {IFace a; uint16 gap; IFace b;}
+
+    struct SG2 {SG1 a; SG b;}
 
     uint public x;
     mapping(uint => mapping(uint => SG)) public data;
     SG public sData;
-
     string public greeting;
     uint256 public other;
 
@@ -38,10 +45,15 @@ contract Greeter is Base1, Base2 {
     mapping(address => uint) var5;
     uint256 e;
 
+    IFace iface;
+    SG1 ifaceStruct;
+    SG2 ifaceStruct2;
+    uint256 e1;
+
     constructor(string memory _greeting) ___constructorVisibility___ Base1(_greeting)  Base2(_greeting) {
         console.log("Deploying a Greeter with greeting:", _greeting);
         greeting = _greeting;
-//>7        _s().var1 = 1;
+        //>7        _s().var1 = 1;
     }
 
     function greet() public view returns (string memory) {
@@ -52,12 +64,12 @@ contract Greeter is Base1, Base2 {
         console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
         greeting = _greeting;
         other = other++;
-//>7        _s().var1 = _s().var1++;
+        //>7        _s().var1 = _s().var1++;
     }
 
-//>7    function _s() internal pure returns (Namespace storage ret)
-//>7    {
-//>7        bytes32 position = NAMESPACE_SLOT;
-//>7        assembly {ret.slot := position}
-//>7    }
+    //>7    function _s() internal pure returns (Namespace storage ret)
+    //>7    {
+    //>7        bytes32 position = NAMESPACE_SLOT;
+    //>7        assembly {ret.slot := position}
+    //>7    }
 }
